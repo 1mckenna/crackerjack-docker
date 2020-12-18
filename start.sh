61ed7d7b297e79e7de6b3453313ed3917408f691
Logan McKenna <mckenna.logan@gmail.com>
2020-12-18 13:48:27 -0600

    make scripts interactive

--- start.sh
+++ start.sh
@@ -3,17 +3,31 @@ WORDLISTDIR=/usr/share/wordlists
 #You should volume mount the below folder so all settings are saved on container stop, start, updates etc...
 # i.e.   docker volume create crackerjack
 #
-#
+
+#Docker Run Command
+dockerCMD="docker run -d --runtime=nvidia -p 4433:443 --mount type=bind,source=$WORDLISTDIR,target=/wordlists --mount source=crackerjack,target=/root/crackerjack/data --name cracker crackerjack:latest"
+
 #Check if container is already created before re-creating
 CID=$(docker ps -q -a -f name=cracker | cut -d ' ' -f1)
 if [ ! -z "$CID" ]
 then
-	echo -n "Starting existing container: "
-	docker start cracker
+	echo "Existing container detected..."
+        read -r -p "Should I Remove the existing container and Recreate [y/N]?" response
+        if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+        then
+	    docker stop $CID >/dev/null
+	    echo -n "Removing container: "
+	    docker rm $CID
+	    echo -n "Creating: "
+	    exec $dockerCMD
+        else
+	    echo -n "Starting existing container: "
+	    docker start cracker
+        fi
 	echo "Done!"
 else
 	echo "Existing Container Not Found..."
 	echo -n "Creating: "
-	docker run -d --runtime=nvidia -p 4433:443 --mount type=bind,source=$WORDLISTDIR,target=/wordlists --mount source=crackerjack,target=/root/crackerjack/data --name cracker crackerjack:latest 
+	exec $dockerCMD
 	echo "Done!"
 fi
--- stop.sh
+++ stop.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-docker stop $(docker ps -q -f name=cracker | cut -d ' ' -f1)
-docker rm cracker
+
+CID=$(docker ps -q -a -f name=cracker | cut -d ' ' -f1)
+if [ ! -z "$CID" ]
+then
+    echo -n "Stopping container: "
+    docker stop $CID
+    read -r -p "Should I Remove the container [y/N]?" response
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+    then
+        echo -n "Removing container: "
+        docker rm $CID
+    fi
+else
+    echo "¯\_(ツ)_/¯ Unable to find the cracker container "
+fi
